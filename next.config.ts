294ca6095dc04341c9530b654a82b977baa04de6
Rayan Reynaldo <rayanvegareynaldo@gmail.com>
2026-01-11 00:16:07 +0800

    Update to nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+module.exports = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "" : "",
+  assetPrefix: isProd ? "" : "",
 };
-
-export default nextConfig;
